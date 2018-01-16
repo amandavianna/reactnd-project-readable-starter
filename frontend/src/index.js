272e7f8c77227a61232b60432f031a86015cbc92
Amanda Vianna <amanda_vianna@hotmail.com>
2018-01-16 00:16:45 -0200

    Create store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,26 @@ import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
+)
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'))
 registerServiceWorker()
